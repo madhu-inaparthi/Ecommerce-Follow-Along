7efb3f3d8c7f2c1fde97301e99cad50ceb0c8ce7
madhu-software <inaparthi.madhukiran.s89@kalvium.community>
2025-05-16 13:56:04 +0530

    done

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -20,6 +20,7 @@ const Products = () => {
   useEffect(() => {
     getData();
   }, []);
+
   return (
     <div className="min-h-screen bg-gray-100 py-8">
       <div className="container mx-auto px-4">
@@ -35,4 +36,5 @@ const Products = () => {
     </div>
   );
 };
+
 export default Products;
